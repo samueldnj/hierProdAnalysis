e3624086a00e9c510f290d74744cffe73f39af5e
sdnjohnson <samueldnjohnson@gmail.com>
2018-10-09 11:30:29 -0700

    Pushing to cray2 for pubBase and DoverAssess

--- multiBatchScript.R
+++ multiBatchScript.R
@@ -14,7 +14,7 @@ source("control.R")
 # List batch file names, base control file names and experiment
 # prefix names (for folder naming)
 # vectors must length k.
-batchFiles    <- c("pubBase_4MPs.bch","DoverAssess.bch")
+batchFiles    <- c("pubBase4MPs.bch","DoverAssess.bch")
 baseCtlFiles  <- c("simCtlFile.txt","simCtlFileDoverAssess.txt")
 expPrefix     <- c("pubBase","DoverAssess")
 plots         <- c(TRUE,TRUE,FALSE,FALSE)
